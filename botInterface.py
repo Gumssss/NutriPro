43abe32a48aa67ae735bc537228ebdca2b3cae8c
elliotcoates <elliotcoateswork@gmail.com>
2026-03-17 14:11:10 +0000

    Bug fix: leaving image empty causes error

--- botInterface.py
+++ botInterface.py
@@ -3,6 +3,8 @@
 #The string that is returned will be output to the user
 
 def suggest_recipes(food_image, meal_type, dietary_restrictions, meal_preferences):    
+    if food_image is None:
+        return "Please upload an image of your ingredients first!"
     #food_image: PIL image object
     #meal_preferences: string
     
